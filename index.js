49355fa5157df6a2aa1034b112aa206568cb836e
Hadil-Guerfi <99086288+Hadil-Guerfi@users.noreply.github.com>
2024-10-17 20:13:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ http
     res.write(`Current Date and Time: ${currentDateTime}`); // Display the current date and time
     res.end(); // End the response
   })
-  .listen(80); // The server object listens on port 80
+  .listen(3000); // The server object listens on port 3000
 
-console.log('Server running on port 80...');
+console.log('Server running on port 3000...');
