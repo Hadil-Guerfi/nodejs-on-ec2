194ba0ee2aaee5c36a8a45de0f6b812501f1055b
Hadil Guerfi <hdlguerfi@gmail.com>
2024-10-17 19:13:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+// Create a server object
+http
+  .createServer(function (req, res) {
+    let currentDateTime = new Date(); // Get the current date and time
+    res.write('Hi from Cloud\n'); // Write the initial response
+    res.write(`Current Date and Time: ${currentDateTime}`); // Display the current date and time
+    res.end(); // End the response
+  })
+  .listen(80); // The server object listens on port 80
+
+console.log('Server running on port 80...');
