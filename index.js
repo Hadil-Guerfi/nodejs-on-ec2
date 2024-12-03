1af7f9449a7863b56b37b0a4c1741d7822fdfd4f
Hadil-Guerfi <99086288+Hadil-Guerfi@users.noreply.github.com>
2024-12-03 02:41:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-var http = require('http');
 
-// Create a server object
-http
-  .createServer(function (req, res) {
     let currentDateTime = new Date(); // Get the current date and time
     console.log("Hiiiiiiiiiiiiiiiiiiiiiiiii",currentDateTime)
-    res.write('Hi from Cloud\n'); // Write the initial response
-    res.write(`Current Date and Time: ${currentDateTime}`); // Display the current date and time
-    res.end(); // End the response
-  })
-  .listen(80); // The server object listens on port 80
-
-console.log('Server running on port 80...');
+  
